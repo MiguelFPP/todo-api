0f6fc3e269f4073aefaf77a5076bd7de4e9bb779
Miguel Perdomo <miguelferneyp@gmail.com>
2024-01-21 18:49:32 -0500

    Add Priority model, factory, migration, and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Priority;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(10)->create();
+        Priority::factory(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
