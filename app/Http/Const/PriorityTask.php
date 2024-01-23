535c48f7517fec3b966fe26ba4944c0b89b308d7
Miguel Perdomo <miguelferneyp@gmail.com>
2024-01-23 18:47:47 -0500

    Add constants for priority, status, and type of tasks

---
+++ app/Http/Const/PriorityTask.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Http\Const;
+
+class PriorityTask {
+    const LOW = 1;
+    const MEDIUM = 2;
+    const HIGH = 3;
+}
---
+++ app/Http/Const/TypeTask.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Http\Const;
+
+class TypeTask {
+    const BUG = 1;
+    const FEATURE = 2;
+    const REFACTOR = 3;
+    const TEST = 4;
+}
