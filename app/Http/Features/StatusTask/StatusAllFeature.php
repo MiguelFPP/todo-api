28290ea7b6c0480cfbded3f35c9f4d06cbbdac8b
Miguel Perdomo <miguelferneyp@gmail.com>
2024-01-23 15:23:56 -0500

    Update API responses with HTTP status codes

--- app/Http/Features/StatusTask/StatusAllFeature.php
+++ app/Http/Features/StatusTask/StatusAllFeature.php
@@ -3,11 +3,12 @@ namespace App\Http\Features\StatusTask;
 
 use App\Http\Resources\StatusTask\StatusAllCollection;
 use App\Models\StatusTask;
+use Symfony\Component\HttpFoundation\Response;
 
 class StatusAllFeature
 {
     public function __invoke()
     {
-        return response()->json(new StatusAllCollection(StatusTask::all()));
+        return response()->json(new StatusAllCollection(StatusTask::all()), Response::HTTP_OK);
     }
 }
--- app/Http/Features/TypeTask/TypesAllFeature.php
+++ app/Http/Features/TypeTask/TypesAllFeature.php
@@ -4,11 +4,12 @@ namespace App\Http\Features\TypeTask;
 
 use App\Http\Resources\TypeTask\TypesAllCollection;
 use App\Models\TypeTask;
+use Symfony\Component\HttpFoundation\Response;
 
 class TypesAllFeature
 {
     public function __invoke()
     {
-        return response()->json(new TypesAllCollection(TypeTask::all()));
+        return response()->json(new TypesAllCollection(TypeTask::all()), Response::HTTP_OK);
     }
 }
