9d142b22345ada399172c2f787f555e8ed1e1098
Miguel Perdomo <miguelferneyp@gmail.com>
2024-01-27 11:11:48 -0500

    add multi del task route api

--- routes/api/task.php
+++ routes/api/task.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Task\AllTaskController;
 use App\Http\Controllers\Task\DeleteTaskController;
 use App\Http\Controllers\Task\GetTaskController;
+use App\Http\Controllers\Task\MultiDelTaskController;
 use App\Http\Controllers\Task\StoreTaskController;
 use App\Http\Controllers\Task\UpdateTaskController;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +14,5 @@ Route::prefix('tasks')->middleware('auth:api')->group(function () {
     Route::delete('/{id}', DeleteTaskController::class)->name('v1.task.delete');
     Route::post('/', StoreTaskController::class)->name('v1.task.store');
     Route::put('/{id}', UpdateTaskController::class)->name('v1.task.update');
+    Route::post('/multiple-delete', MultiDelTaskController::class)->name('v1.task.multiple-delete');
 });
